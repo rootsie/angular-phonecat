893e4ae840b8a54a26caa1985b5be7cec98da51d
rootsie <moncefdridi@gmail.com>
2014-03-05 00:16:00 +0100

    step-10 Experiments
    Add function to controller

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -18,7 +18,10 @@ phonecatControllers.controller('PhoneDetailCtrl', ['$scope', '$routeParams', '$h
     $http.get('phones/' + $routeParams.phoneId + '.json').success(function(data) {
       $scope.phone = data;
       $scope.mainImageUrl = data.images[0];
-    });
+      $scope.hello = function(name) {
+        alert('Hello ' + (name || 'world') + '!');
+      }    
+      });
 
     $scope.setImage = function(imageUrl) {
       $scope.mainImageUrl = imageUrl;
